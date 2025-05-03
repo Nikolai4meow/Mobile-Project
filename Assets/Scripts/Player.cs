dd3e30581e71d542581f2967b99dbd1188d1bf4c
miladonn <miladon1173@gmail.com>
2025-05-03 14:53:15 +0300

    BasicMovementSwipe

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed;
+    private Vector3 targetPos;
+
+     void Start()
     {
-        
+        targetPos = transform.position;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector3 moveDirection)
     {
-        
+        targetPos += moveDirection;
     }
+
+     void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
 }
--- Assets/Scripts/PlayerSwipe.cs
+++ Assets/Scripts/PlayerSwipe.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class PlayerSwipe : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Player player;
+    private Vector2 startPos;
+    public int pixelDistance = 20;
+    private bool isFingerDown;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isFingerDown && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) // detects if finger is down for the first time 
+        {
+            startPos = Input.touches[0].position; // startPos is where we put our finger down
+            isFingerDown = true; //the finger is down on the screen
+        }
+
+        if (isFingerDown)
+        {
+            if (Input.touches[0].position.y >= startPos.y + pixelDistance)
+            {
+                isFingerDown = false;
+                Debug.Log("Up");
+                player.Move(Vector3.forward);
+            }
+            else if (Input.touches[0].position.y <= startPos.y - pixelDistance)
+            {
+                isFingerDown = false;
+                Debug.Log("Down");
+                player.Move(-Vector3.forward);
+            }
+            else if (Input.touches[0].position.x <= startPos.x - pixelDistance)
+            {
+                isFingerDown = false;
+                Debug.Log("Left");
+                player.Move(Vector3.left);
+            }
+            else if (Input.touches[0].position.x >= startPos.x + pixelDistance)
+            {
+                isFingerDown = false;
+                Debug.Log("Right");
+                player.Move(Vector3.right);
+            }
+
+        }
     }
 }
