c1c39c19c8e05a2c626a3ded694dac170ac345b0
miladonn <55191447+miladonn@users.noreply.github.com>
2025-05-20 15:17:53 +0300

    FinishFlag added

---
+++ Assets/Scripts/FinishFlagWin.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FinishFlagWin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("YOU WON!");
+        }
+    }
+}
